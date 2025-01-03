da2ab672bbba2eb02e460610675a75b25717f846
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2025-01-04 01:05:46 +0200

    desperation2

--- server/routes/MessagesRoutes.js
+++ server/routes/MessagesRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 import { getMessages } from "../controllers/MessagesController.js";
-import multer from "multer";
+// import multer from "multer";
 
 const messagesRoutes = Router();
-const upload = multer({ dest: "uploads/files" });
+// const upload = multer({ dest: "uploads/files" });
 
 messagesRoutes.post("/get-messages", verifyToken, getMessages);
 // messagesRoutes.post(
