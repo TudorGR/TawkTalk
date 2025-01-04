f5f7b6d21d05386138932f03d15098e8214e4494
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2025-01-04 12:15:48 +0200

    debugging

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -46,32 +46,40 @@ export const lol = async (req, res, next) => {
 };
 
 export const login = async (req, res, next) => {
+  // try {
+  //   const { email, password } = req.body;
+  //   if (!email || !password) {
+  //     return res.status(400).send("Email and password is required");
+  //   }
+  //   const user = await User.findOne({ email });
+  //   if (!user) {
+  //     return res.status(400).send("User with this email doesn't exist");
+  //   }
+  //   const auth = await compare(password, user.password);
+  //   if (!auth) {
+  //     return res.status(400).send("Password incorrect");
+  //   }
+  //   res.cookie("jwt", createToken(email, user.id), {
+  //     maxAge,
+  //   });
+  //   return res.status(200).json({
+  //     user: {
+  //       id: user.id,
+  //       email: user.email,
+  //       profileSetup: user.profileSetup,
+  //       firstName: user.firstName,
+  //       lastName: user.lastName,
+  //       image: user.image,
+  //       color: user.color,
+  //     },
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  //   return res.status(500).send("Internal Server Error");
+  // }
   try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).send("Email and password is required");
-    }
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).send("User with this email doesn't exist");
-    }
-    const auth = await compare(password, user.password);
-    if (!auth) {
-      return res.status(400).send("Password incorrect");
-    }
-    res.cookie("jwt", createToken(email, user.id), {
-      maxAge,
-    });
-    return res.status(200).json({
-      user: {
-        id: user.id,
-        email: user.email,
-        profileSetup: user.profileSetup,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        image: user.image,
-        color: user.color,
-      },
+    return res.status(201).json({
+      message: "hello",
     });
   } catch (error) {
     console.log(error);
