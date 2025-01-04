5844d0960b3db87f64d98da9978e5a587360fd16
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2025-01-04 12:17:59 +0200

    no cookie + msg

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -46,39 +46,32 @@ export const lol = async (req, res, next) => {
 };
 
 export const login = async (req, res, next) => {
-  // try {
-  //   const { email, password } = req.body;
-  //   if (!email || !password) {
-  //     return res.status(400).send("Email and password is required");
-  //   }
-  //   const user = await User.findOne({ email });
-  //   if (!user) {
-  //     return res.status(400).send("User with this email doesn't exist");
-  //   }
-  //   const auth = await compare(password, user.password);
-  //   if (!auth) {
-  //     return res.status(400).send("Password incorrect");
-  //   }
-  //   res.cookie("jwt", createToken(email, user.id), {
-  //     maxAge,
-  //   });
-  //   return res.status(200).json({
-  //     user: {
-  //       id: user.id,
-  //       email: user.email,
-  //       profileSetup: user.profileSetup,
-  //       firstName: user.firstName,
-  //       lastName: user.lastName,
-  //       image: user.image,
-  //       color: user.color,
-  //     },
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  //   return res.status(500).send("Internal Server Error");
-  // }
   try {
-    return res.status(201).json({
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).send("Email and password is required");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send("User with this email doesn't exist");
+    }
+    const auth = await compare(password, user.password);
+    if (!auth) {
+      return res.status(400).send("Password incorrect");
+    }
+    // res.cookie("jwt", createToken(email, user.id), {
+    //   maxAge,
+    // });
+    return res.status(200).json({
+      // user: {
+      //   id: user.id,
+      //   email: user.email,
+      //   profileSetup: user.profileSetup,
+      //   firstName: user.firstName,
+      //   lastName: user.lastName,
+      //   image: user.image,
+      //   color: user.color,
+      // },
       message: "hello",
     });
   } catch (error) {
