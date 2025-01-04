260d1ad602a10f8f60a220dfe3264c0a9e75e103
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2025-01-04 15:21:32 +0200

    fa

--- server/index.js
+++ server/index.js
@@ -39,10 +39,10 @@ app.use("/api/messages", messagesRoutes);
 app.use("/api/channel", channelRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
 
