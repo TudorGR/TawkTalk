86783defa12d9ac78900c6be73457f3db4a0494c
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2025-01-04 11:31:06 +0200

    .

--- server/server.js
+++ server/server.js
@@ -15,15 +15,18 @@ const app = express();
 const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://tawk-talk-server.vercel.app"],
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+app.use(cors());
 
-app.use(cors(corsOptions));
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
 
 app.use("/uploads/profiles", express.static("uploads/profiles"));
 app.use("/uploads/files", express.static("uploads/files"));
