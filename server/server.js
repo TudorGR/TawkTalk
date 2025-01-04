1f45f15d5946fd0599a479e27174faf09947900e
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2025-01-04 13:35:20 +0200

    aa

--- server/server.js
+++ server/server.js
@@ -1,58 +1,79 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import cookieParser from "cookie-parser";
+import "dotenv/config";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/AuthRoutes.js";
 import contactsRoutes from "./routes/ContactRoutes.js";
 import messagesRoutes from "./routes/MessagesRoutes.js";
 import channelRoutes from "./routes/ChannelRoutes.js";
 import setupSocket from "./socket.js";
+import connectDB from "./config/mongodb.js";
 
 dotenv.config();
-const databaseURL = process.env.DATABASE_URL;
 
 const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
 
-const startServer = async () => {
-  try {
-    await mongoose.connect(`${databaseURL}`);
-    console.log("DB connection Successful");
+app.use(express.json());
+app.use(cors());
 
-    const port = process.env.PORT || 4000;
+app.use(express.json());
+app.use(cookieParser());
 
-    app.use(
-      cors({
-        origin: [process.env.ORIGIN, "https://tawk-talk-server.vercel.app"],
-        credentials: true,
-      })
-    );
-    app.use(express.json());
-    app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactsRoutes);
+app.use("/api/messages", messagesRoutes);
+app.use("/api/channel", channelRoutes);
 
-    // app.use("/uploads/profiles", express.static("uploads/profiles"));
-    // app.use("/uploads/files", express.static("uploads/files"));
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-    app.use("/api/auth", authRoutes);
-    app.use("/api/contacts", contactsRoutes);
-    app.use("/api/messages", messagesRoutes);
-    app.use("/api/channel", channelRoutes);
+const server = app.listen(port, () =>
+  console.log("server started on port : " + port)
+);
+setupSocket(server);
 
-    app.get("/", (req, res) => {
-      res.send("API Working");
-    });
+// const startServer = async () => {
+//   try {
+//     await mongoose.connect(`${databaseURL}`);
+//     console.log("DB connection Successful");
 
-    const server = app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`);
-    });
+//     app.use(
+//       cors({
+//         origin: [process.env.ORIGIN, "https://tawk-talk-server.vercel.app"],
+//         credentials: true,
+//       })
+//     );
+//     app.use(express.json());
+//     app.use(cookieParser());
 
-    setupSocket(server);
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1);
-  }
-};
+//     // app.use("/uploads/profiles", express.static("uploads/profiles"));
+//     // app.use("/uploads/files", express.static("uploads/files"));
 
-startServer();
+//     app.use("/api/auth", authRoutes);
+//     app.use("/api/contacts", contactsRoutes);
+//     app.use("/api/messages", messagesRoutes);
+//     app.use("/api/channel", channelRoutes);
 
-export default app;
+//     app.get("/", (req, res) => {
+//       res.send("API Working");
+//     });
+
+//     const server = app.listen(port, () => {
+//       console.log(`Server is running at http://localhost:${port}`);
+//     });
+
+//     setupSocket(server);
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// startServer();
+
+// export default app;
